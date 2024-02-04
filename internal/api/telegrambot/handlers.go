9f0763f43a2089186248678b2cd249bf79b75d5b
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-02-04 22:29:17 +0300

    refactor tg handlers

--- internal/api/telegrambot/handlers.go
+++ internal/api/telegrambot/handlers.go
@@ -8,14 +8,9 @@ import (
 	"merger-adapter/internal/api/telegrambot/deffereduploader"
 	mrepo "merger-adapter/internal/repository/messages_repository"
 	"merger-adapter/internal/service/merger"
-	"time"
 )
 
-var lastReq = time.Now()
-
 func (c *Client) onTelegramCreatedMessage(b *gotgbot.Bot, ctx *ext.Context) error {
-	log.Printf("onTelegramCreatedMessage: after last %d ms ", time.Now().UnixMilli()-lastReq.UnixMilli())
-	lastReq = time.Now()
 	err := c.du.Upload(*ctx.Message)
 	if err != nil {
 		return fmt.Errorf("upload msg to IDeferredUploader: %s", err)
@@ -29,20 +24,20 @@ func (c *Client) listenServerMessages() error {
 		if err != nil {
 			return fmt.Errorf("receive update: %s", err)
 		}
-		go c.onMergerMessage(msg)
+		go onMergerMessage(msg, c.bot, c.repo, c.chatID)
 	}
 }
 
-func (c *Client) onMergerMessage(msg *merger.Message) {
+func onMergerMessage(msg *merger.Message, bot *gotgbot.Bot, repo mrepo.MessagesRepository, chatID int64) {
 	// reply
-	replyTo, err := replyTgIdFromMsg(msg, c.repo, c.chatID)
+	replyTo, err := replyTgIdFromMsg(msg, repo, chatID)
 	if err != nil {
 		log.Printf("[ERROR] replyTgIdFromMsg: %s", err)
 		replyTo = 0
 	}
 
-	tgMsg, err := c.bot.SendMessage(
-		c.chatID,
+	tgMsg, err := bot.SendMessage(
+		chatID,
 		msg.FormatShort(),
 		&gotgbot.SendMessageOpts{ReplyParameters: &gotgbot.ReplyParameters{ChatId: replyTo}},
 	)
@@ -50,8 +45,16 @@ func (c *Client) onMergerMessage(msg *merger.Message) {
 		log.Printf("[ERROR] send message to tg: %s", err)
 		return
 	}
+	err = saveToRepo(repo, msg, tgMsg)
+	if err != nil {
+		log.Printf("[ERROR] save message to repo: %s", err)
+		return
+	}
+}
+
+func saveToRepo(repo mrepo.MessagesRepository, msg *merger.Message, tgMsg *gotgbot.Message) error {
 	kind := deffereduploader.DefineKind(*tgMsg)
-	err = c.repo.Add(mrepo.Message{
+	return repo.Add(mrepo.Message{
 		ReplyMergerMsgId: msg.ReplyId,
 		MergerMsgId:      msg.Id,
 		ChatId:           tgMsg.Chat.Id,
@@ -62,10 +65,6 @@ func (c *Client) onMergerMessage(msg *merger.Message) {
 		HasMedia:         kind == deffereduploader.Media || kind == deffereduploader.GroupMedia,
 		CreatedAt:        tgMsg.Date,
 	})
-	if err != nil {
-		log.Printf("[ERROR] add msg to repos: %s", err)
-		return
-	}
 }
 
 func replyTgIdFromMsg(msg *merger.Message, repo mrepo.MessagesRepository, chatId int64) (int64, error) {
